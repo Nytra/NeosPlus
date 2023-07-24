f2880432f73535da32e0c84665794f3cc4fb7f46
Sam Scott <samueltscott@gmail.com>
2023-07-24 19:15:05 +0100

    More testing

--- NEOSPlus/Logix/Assets/SetStaticTexture2DPixel.cs
+++ NEOSPlus/Logix/Assets/SetStaticTexture2DPixel.cs
@@ -3,6 +3,9 @@ using FrooxEngine.LogiX;
 using BaseX;
 using System;
 using FrooxEngine.UIX;
+using System.Drawing.Drawing2D;
+using System.Threading.Tasks;
+using CodeX;
 
 [Category(new string[] { "LogiX/Assets" })]
 public class SetStaticTexture2DPixel : LogixNode
@@ -32,21 +35,68 @@ public class SetStaticTexture2DPixel : LogixNode
         {
             if (texture != null)
             {
-                //Action<IChangeable> func = null;
-                //func = delegate
-                //{
-                //    OnDone.Trigger();
-                //    texture.URL.Changed -= func;
-                //};
-                //texture.URL.Changed += func;
+                //color existingColor = texture.Asset.Data.GetPixel(position.x, position.y, mipLevel);
+                //color existingColor = texture.Asset.GetOriginalTextureData().GetPixel(position.x, position.y, mipLevel);
+                StartTask(async delegate
+                {
+                    OnStarted.Trigger();
 
-                OnStarted.Trigger();
-                texture.Process((B) => { B.SetPixel(position.x, position.y, c, mipLevel); return B; }, null);
-                OnDone.Trigger();
+                    bool flag = false;
+
+                    //UniLog.Log("Hello!");
+                    try
+                    {
+                        Task<Bitmap2D> thing = texture.Asset.GetOriginalTextureData();
+                        //UniLog.Log("a");
+                        await thing;
+                        //UniLog.Log("b");
+                        color existingColor = thing.Result.GetPixel(position.x, position.y, mipLevel);
+                        //UniLog.Log("c");
+                        //UniLog.Log("existingColor: " + existingColor.ToString());
+                        if (existingColor != c)
+                        {
+                            //Action<IChangeable> func = null;
+                            //func = delegate
+                            //{
+                            //    OnDone.Trigger();
+                            //    texture.URL.Changed -= func;
+                            //};
+                            //texture.URL.Changed += func;
+                        }
+                        else
+                        {
+                            flag = true;
+                        }
+
+                        if (flag)
+                        {
+                            OnDone.Trigger();
+                        }
+                        else
+                        {
+                            //UniLog.Log("Hello2!");
+                            texture.Process((B) => { B.SetPixel(position.x, position.y, c, mipLevel); return B; }, null);
+                            while (texture.Asset == null)
+                            {
+                                // wait until asset available
+                                await Task.Delay(25);
+                            }
+                            OnDone.Trigger();
+                        }
+                        //UniLog.Log("Hello3!");
+                    }
+                    catch (Exception e)
+                    {
+                        UniLog.Error("Error inside try catch 2");
+                        UniLog.Error(e.ToString());
+                        OnFail.Trigger();
+                    }
+                });
 
                 // TODO:
-                // Make OnDone trigger when the asset URL changes or if the pixel color is already matching the input color then it can pulse immediately
-                // Basically it should only pulse when the asset actually updated (if it needed to update)
+                // OnDone fires too early?
+                // Needs extra updates delay after OnDone to not cause errors
+                // Maybe waiting for Asset URL to change is not enough?
             }
             else
             {
@@ -55,6 +105,7 @@ public class SetStaticTexture2DPixel : LogixNode
         }
         catch (Exception e)
         {
+            UniLog.Error("Error inside try catch 1");
             UniLog.Error(e.ToString());
             OnFail.Trigger();
         }
