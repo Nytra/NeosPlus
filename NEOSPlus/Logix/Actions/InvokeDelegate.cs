19b04312fc7924c16ce8d36304b905223b1e743d
Sam Scott <samueltscott@gmail.com>
2023-07-24 14:47:06 +0100

    Add Invoke Delegate node

---
+++ NEOSPlus/Logix/Actions/InvokeDelegate.cs
@@ -0,0 +1,22 @@
+﻿using FrooxEngine;
+using FrooxEngine.LogiX;
+
+[NodeName("Invoke Delegate")]
+[Category(new string[] { "LogiX/Actions" })]
+public class InvokeDelegate : LogixNode
+{
+    public readonly Input<ISyncDelegate> Target;
+
+    public readonly Impulse OnDone;
+
+    [ImpulseTarget]
+    public void Invoke()
+    {
+        ISyncDelegate syncDelegate = Target.EvaluateRaw();
+        if (syncDelegate != null && syncDelegate.Method != null)
+        {
+            syncDelegate.Method.DynamicInvoke();
+            OnDone.Trigger();
+        }
+    }
+}
