16807c87df0913b096d1b4d3ab9ed5538026a0f3
Sam Scott <samueltscott@gmail.com>
2023-07-24 14:48:49 +0100

    Add Construct RefID node

---
+++ NEOSPlus/Logix/References/ConstructRefID.cs
@@ -0,0 +1,22 @@
+﻿using FrooxEngine;
+using FrooxEngine.LogiX;
+using BaseX;
+
+[Category(new string[] { "LogiX/References" })]
+[NodeName("Construct RefID")]
+public class ConstructRefID : LogixOperator<RefID>
+{
+    public readonly Input<ulong> Position;
+
+    public readonly Input<byte> User;
+
+    public override RefID Content
+    {
+        get
+        {
+            ulong position = Position.EvaluateRaw();
+            byte user = User.EvaluateRaw();
+            return RefID.Construct(position, user);
+        }
+    }
+}
